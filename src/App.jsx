1e37b744197dfe06801f4cb18de89618a2dabf8c
hannut91 <hannut91@gmail.com>
2024-06-04 16:59:15 +0900

    Add Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+
+import { Link } from "react-router-dom";
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -32,6 +35,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Link to="/home">Go to Home!</Link>
     </>
   )
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,9 +3,14 @@ import { test, expect } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/react';
 
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
 test('카운트를 클릭하면, count가 증가한다.', () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
 
   fireEvent.click(screen.getByText('count is 0'));
 
