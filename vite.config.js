45f95f86904d9aa3cf4517135e33134f8c4f0682
hannut91 <hannut91@gmail.com>
2024-06-11 17:17:00 +0900

    Deploy s3 workflow

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,29 @@
+import fs from 'fs/promises';
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'move-about-html',
+      apply: 'build',
+      writeBundle: async () => {
+        const aboutSourcePath = 'dist/pages/about/index.html'
+        const aboutDestPath = 'dist/about/index.html'
+
+        try {
+          await fs.mkdir('dist/about', { recursive: true })
+          await fs.rename(aboutSourcePath, aboutDestPath)
+          console.log(`Moved: ${aboutSourcePath} -> ${aboutDestPath}`)
+        } catch (err) {
+          console.error('Error moving about/index.html:', err)
+        }
+      }
+    }
+  ],
   test: {
     environment: 'jsdom',
   },
@@ -11,7 +31,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
-        about: 'about.html',
+        'about/index': 'pages/about/index.html',
       }
     }
   }
